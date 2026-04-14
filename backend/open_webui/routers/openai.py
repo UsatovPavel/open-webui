0e5d4d13389f07d29e0d5772a27130dea203e4d3
Usatov Pavel <dbpfynbqcrfzbvgthbz@gmail.com>
2026-04-14 18:48:34 +0300

    add message_id put

--- backend/open_webui/routers/openai.py
+++ backend/open_webui/routers/openai.py
@@ -38,6 +38,7 @@ from open_webui.env import (
     AIOHTTP_CLIENT_TIMEOUT_MODEL_LIST,
     ENABLE_FORWARD_USER_INFO_HEADERS,
     FORWARD_SESSION_INFO_HEADER_CHAT_ID,
+    FORWARD_SESSION_INFO_HEADER_MESSAGE_ID,
     BYPASS_MODEL_ACCESS_CONTROL,
 )
 from open_webui.models.users import UserModel
@@ -164,6 +165,8 @@ async def get_headers_and_cookies(
         headers = include_user_info_headers(headers, user)
         if metadata and metadata.get('chat_id'):
             headers[FORWARD_SESSION_INFO_HEADER_CHAT_ID] = metadata.get('chat_id')
+        if metadata and metadata.get('message_id'):
+            headers[FORWARD_SESSION_INFO_HEADER_MESSAGE_ID] = metadata.get('message_id')
 
     token = None
     auth_type = config.get('auth_type')
